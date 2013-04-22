d424b4c39ae9ca3d0e86c9cd799b6c36925cdeed
Dom Nziaka <sdnziaka@gmail.com>
2013-04-23 00:09:02 +0100

    add: estrtura inicial do site
    Adicionados pastas para imagens, estilos, scripts, etc e o aqruivo
    index.php para página inicial.

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+<title> Remake do Site Oficial do Mundial de Hóquei Angola 2013 </title>
+<link rel="" href="" >
+</head>
+<body>
+<div class="wrapper">
+<header>
+	<h1></h1>
+	<nav>
+    	<ul id="nav-list">
+        	<li></li>
+        </ul>
+    </nav>
+</header>
+<div id="main-container">
+</div>
+<footer>&copy; Simão Dom Nziaka</footer>
+</div>
+
+<script src=""></script>
+
+</body>
+</html>
